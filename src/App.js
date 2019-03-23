09e22ca0f9d1e49568b3a1830c613a3543d78272
Julie Broderick <broderick.julie@gmail.com>
2019-03-23 14:21:55 -0500

    started work on handleAddCard

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
 import React, { Component } from 'react';
 import List from './List'
+import STORE from './STORE'
 import './App.css';
+
+const newRandomCard = () => {
+  const id = Math.random().toString(36).substring(2, 4)
+    + Math.random().toString(36).substring(2, 4);
+  return {
+    id,
+    title: `Random Card ${id}`,
+    content: 'lorem ipsum',
+  }
+}
 
 class App extends Component {
   static defaultProps = {
@@ -10,19 +21,57 @@ class App extends Component {
     }
   };
 
+  state = {
+    store: STORE,
+
+  }
+
+  handleDeleteItem() {
+    console.log('clicked delete');
+  }
+
+  handleAddCard = (id) => {
+    console.log('id', id);
+    const randomCardInfo = newRandomCard()
+          
+    // let something = this.state.store.lists.map(item => {
+    //   if (item.id === id) {
+    //     console.log('item.id',item.id);
+    //     console.log(item.cardIds);
+
+    //       [item.cardIds]: [...item.carIds, [randomCardInfo.id]]
+    //   }
+
+    // })
+  
+    this.setState({
+      store: {
+        allCards: {
+          ...this.state.store.allCards,
+          [randomCardInfo.id]: randomCardInfo
+        },
+        lists: something
+      }
+    })
+       
+  }
+  
   render() {
-    const { store } = this.props
+        
     return (
       <main className='App'>
         <header className='App-header'>
           <h1>Trelloyes!</h1>
         </header>
         <div className='App-list'>
-          {store.lists.map(list => (
+          {this.state.store.lists.map(list => (
             <List
               key={list.id}
+              id={list.id}
               header={list.header}
-              cards={list.cardIds.map(id => store.allCards[id])}
+              cards={list.cardIds.map(id => this.state.store.allCards[id])}
+              onAddCard={this.handleAddCard}
+              onDeleteItem={this.handleDeleteItem}
             />
           ))}
         </div>
